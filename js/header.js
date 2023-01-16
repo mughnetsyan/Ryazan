63a03d86813ebe3daf84b6016f461288a6edce98
FRONTfikry <edvard2027@gmail.com>
2023-01-16 21:22:25 +0300

    Added animation to header dropdown content

--- js/header.js
+++ js/header.js
@@ -1,8 +1,23 @@
-document.querySelector('#dropdown__btn').addEventListener('click', function (e) {
-    var dropdownContent = document.querySelector('#dropdown__content')
-    var dropdownImg = document.querySelector('#dropdown__img')
-    dropdownContent.style.opacity = dropdownContent.style.opacity == '1' ? '0' : '1';
-    dropdownContent.style.display = dropdownContent.style.display == 'grid' ? 'none' : 'grid';
+let dropdownContent = document.querySelector('#dropdown__content');
+let dropdownImg = document.querySelector('#dropdown__img');
 
-    dropdownImg.style.transform = dropdownImg.style.transform == "rotateX(180deg)" ? "rotateX(0deg)" : "rotateX(180deg)"
+let dropdownBtn = document.querySelector("#dropdown__btn");
+
+dropdownBtn.addEventListener('click', function() {
+    if(dropdownContent.classList.contains("d-none")) {
+
+        dropdownContent.classList.remove("d-none")
+        setTimeout(function() {
+            dropdownContent.classList.remove("opacity-0")
+        }, 0)
+
+        dropdownImg.classList.add("rotateX-180deg")
+    } else {
+        dropdownContent.classList.add("opacity-0")
+        setTimeout(function() {
+            dropdownContent.classList.add("d-none")
+        }, 300)
+
+        dropdownImg.classList.remove("rotateX-180deg")
+    }
 })
